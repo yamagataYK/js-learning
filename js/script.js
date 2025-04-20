e6e6cce9b95b55e1aa642d22e30d051ba2b2c509
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-04-20 18:33:31 +0900

    todoリストの追加、削除、完了のコーディング

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach(todo => {
+        add(todo);
+    })
+}
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    add();
+});
+
+function add(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
+    }
+
+    if (todoText.length > 0) {
+        const li = document.createElement("li");
+        li.innerText = todoText;
+        li.classList.add("list__group-item");
+
+        if (todo && todo.completed) {
+            li.classList.add("text-completion")
+        }
+
+        // 右クリック削除
+        li.addEventListener("contextmenu", function
+            (event) {
+            event.preventDefault();
+            li.remove();
+            saveData();
+        });
+
+        li.addEventListener('click', function () {
+            li.classList.toggle("text-completion");
+            saveData();
+        });
+
+        ul.appendChild(li)
+        input.value = "";
+        saveData();
+    }
+}
+
+//ローカルストレージに保管
+function saveData() {
+    const lists = document.querySelectorAll("li");
+    let todos = [];
+    lists.forEach(list => {
+        let todo = {
+            text: list.innerText,
+            completed: list.classList.contains
+                ("text-completion")
+        };
+        todos.push(todo);
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
