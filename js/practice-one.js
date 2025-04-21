9fd97a4390255405ef183f3bcaebbccec977e55d
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-04-21 23:58:49 +0900

    コード修正

--- js/practice-one.js
+++ js/practice-one.js
@@ -1,29 +1,52 @@
 const video = document.getElementById('camera');
+const canvas = document.getElementById("snapshot");
+const photo = document.getElementById("photo");
 const stopBtn = document.getElementById('stopBtn');
 const startBtn = document.getElementById('startBtn');
+const captureBtn = document.getElementById("captureBtn");
+
 let stream;
+//↑あとで使うための変数を先に宣言している Webカメラの映像データ（ストリーム）を保持する変数を準備
 
 async function startCamera() {
-    try {
-        stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-        video.srcObject = stream;
-    } catch (error) {
-        console.error('カメラへのアクセスに失敗しました:', error);
-    }
+    //↑非同期関数[async function]
+    stream = await navigator.mediaDevices.getUserMedia({
+        //↑カメラを使う許可をブラウザにリクエストしている
+        video: true, audio: false
+    });
+    //↑映像は必要だけど音声はいらない
+    video.srcObject = stream;
+    //<video> タグにカメラ映像を映している
 }
 
-// 初回起動時にカメラON
-startCamera();
 
 stopBtn.addEventListener('click', () => {
     if (stream) {
+        //カメラが起動中なら実行する
         stream.getTracks().forEach(track => track.stop());
+        //映像をトラックで配列で返してる
         video.srcObject = null;
     }
 });
 
+
 startBtn.addEventListener('click', () => {
     if (!stream || !stream.active) {
+        //!stream  streamがまだ存在してない ||または  active はカメラが止まってる どちらか二つなら
         startCamera();
+        //上で書いた非同期関数を呼び出している
     }
 });
+
+captureBtn.addEventListener("click", () => {
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    //キャンバスのサイズをビデオタグの解像度に合わしている
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageDataUrl = canvas.toDataURL('image/png');
+    photo.src = imageDataUrl;
+});
+
