2a96227a7279fe35510266f69efbf145fca220ed
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-04-23 19:36:25 +0900

    if swich文の復習

---
+++ js/practice.js
@@ -0,0 +1,64 @@
+ok = true && false;
+// && かつ
+ok = true || false
+// ||　または　
+
+const userInput = '';
+const userName = userInput || 'user';
+// ログインなどで、userInputに何も入れない時、左側がfalseとなり、右を返す
+
+ok = x = 15;
+ok = x > 10 && x < 20;
+
+{
+    const hi = 'hello';
+    {
+
+        console.log(hi);
+    }
+}
+
+if (ok) {
+
+} else
+    ok = false
+ok = ok ? 'ok' : 'No';
+
+// if文でかいたバージョン
+
+// function vegetableColor(vegetable) {
+//     if (vegetable === 'tomato') {
+//         console.log('tomato is red');
+
+//     } else if (vegetable === 'pumpkin') {
+//         console.log('pum is orange');
+//     } else if (vegetable === 'onion') {
+//         console.log('onion is white');
+//     }
+// }
+// vegetableColor('pumpkin')
+
+function vegetableColor(vegetable) {
+    switch (vegetable) {
+        case 'tomato': {
+            const message = 'tomato is red';
+            console.log(message);
+            break;
+        }
+        case 'carrot':
+        case 'pumpkin': {
+            const message = `${vegetable} is orange`;
+            break;
+        }
+        case 'onion': {
+            console.log('onion is white');
+            break;
+        }
+        default: {
+            const message = 'not found'
+            console.log('not found');
+        }
+
+    }
+}
+vegetableColor('tomato')
