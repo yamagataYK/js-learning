5132128ffc50c13a968e4860625b87a3adf24322
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-04-26 04:47:55 +0900

    try catch文について

--- js/practice.js
+++ js/practice.js
@@ -69,14 +69,57 @@ while (count < 10) {
     count += 1;
 }
 
-let tomtoCount = 100;
+let tomatoCount = 100;
 do {
     tomatoCount += 1;
 } while (tomatoCount < 10);
 
 
+// for文
+// for (let pumpkinCount = 0, i = 0, ; pumpkinCount < 10; pumpkinCount += 1) {
 
-for (let pumpkinCount = 0, i = 0, ; pumpkinCount < 10; pumpkinCount += 1) {
+// }
 
+const fruits = ['apple', 'banana', 'grape']
+// for (let i = 0; i < fruits.length; i += 0) {
+// }  for文でやってみたver
+
+//for of文
+for (const fruit of fruits) {
+    // console.log(fruits);
 }
+
+const coffee = {
+    name: "cafe",
+    size: 350,
+    isHot: true,
+}
+
+for (const key in coffee) {
+    console.log(coffee[key]);
+    if (key === 'size') {
+        continue;
+    }
+}
+
+const cho = 1
+try {
+    console.log(cho);
+} catch {
+    console.log("文字が入力されていません");
+}
+
+//finally throw  (error)
+function chocolate() {
+    try {
+        // throw 'error';
+        console.log(choco);
+        console.log('try');
+    } catch (error) {
+        console.log("catch", error);
+    } finally {
+        console.log("finally");
+    }
+}
+chocolate()
 
