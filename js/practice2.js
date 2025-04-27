168331ea1b20a585c23b9724b602f67d5b4b88a7
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-04-27 14:56:44 +0900

    コールバック関数

---
+++ js/practice2.js
@@ -0,0 +1,59 @@
+function add(a, d) {
+    return a + b;
+}
+
+//関数式
+let sayHi = function hi() {
+    return "hi";
+}
+// console.log(sayHi);
+
+sayHi = function () {
+
+}
+
+const person = {
+    name: 'yosiki',
+    Hi: function () {
+        return 'hi';
+    }
+};
+
+//アロー関数
+sayHi = (name) => {
+    return `Hi ${name}!`;
+}
+
+//一つの式しか返さない場合の省略
+// sayHi = (name) => `Hi ${name}!`;
+
+
+sayHi = (name, message = 'i like cake') => `Hi ${name}! ${message}`;
+
+console.log(sayHi('kaito'));
+
+//無限引数
+let sum = (...nums) => {
+    let total = 0;
+    for (num of nums) {
+        total += num;
+    }
+    return total;
+};
+// console.log(sum(1., 3, 5, 3));
+
+//コールバック関数
+let subtrack = (a, b, callback) => {
+    let result = a - b;
+    callback('hello');
+};
+subtrack(10, 3, (result) => {
+    console.log(result);
+
+});
+
+subtrack(10, 4, (result) => {
+    alert(result);
+
+});
+
