b938cc563af09439574dc3ef97037d8c48bc6138
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-04-29 02:56:02 +0900

    getselection

--- js/webAPI.js
+++ js/webAPI.js
@@ -1,5 +1,25 @@
 // alert('hello')
 // let result = confirm('thankyou')
-let result = prompt('name', 'ここにかけ')
-console.log(result);
+// let result = prompt('name', 'ここにかけ')
+// console.log(result);
+
+let url = new URL('https://www.udemy.com/');
+console.log(url);
+
+let timer = setTimeout(() => {
+    console.log("hello");
+
+}, 2000);
+clearTimeout(timer)
+
+let timeID = setInterval(() => {
+    console.log("hello");
+
+}, 1000);
+setTimeout(() => {
+    clearInterval(timeID)
+}, 5000);
+
+getSelection().toString()
+
 
