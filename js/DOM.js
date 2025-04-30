3cb0b9bbec2c8d71c15368aa6ae5f973d399946f
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-04-30 23:56:34 +0900

    dom

---
+++ js/DOM.js
@@ -0,0 +1,18 @@
+let result = document.documentElement;
+// result = document.head;
+
+const Tit = document.querySelector("#title")
+const p = document.querySelectorAll("p")
+
+
+console.log(Tit);
+console.log(p);
+
+result = document.links;
+console.log(result);
+
+result = document.body.innerHTML;
+
+console.log(result);
+
+let pp = document.createElement('p');
