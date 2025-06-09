3537bf04549b7990adea384f1560e74624d9d905
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-06-10 04:12:08 +0900

    チェックボックスだけ削除

--- js/practice3.js
+++ js/practice3.js
@@ -4,15 +4,62 @@ const deleteBtn = document.querySelector(".input-group__deletebtn");
 const itemlist = document.querySelector(".standard-item__list")
 
 
+//ローカリストレージのデータ取得とjSONデータオブジェクトもどす
+function getItems() {
+    const data = localStorage.getItem("standard-item");
+    return data ? JSON.parse(data) : [];
+}
 
+//保存 JSONデータ文字列へ変換
+function saveItems(items) {
+    localStorage.setItem("standard-item", JSON.stringify(items));
+}
+//最初の画面
+function renderInitialList() {
+    const items = getItems();
+    items.forEach(item => {
+        itemlist.insertAdjacentHTML(
+            "afterbegin",
+            `<li>
+            <input type="checkbox" class="item-checkbox">
+            ${item}</li>`)
+    })
+}
+
+//保存ボタンをおしたときのやつ
 saveBtn.addEventListener("click", () => {
-    const inputData = inputForm.value;
-    localStorage.setItem("standard-item", inputData);
+    const inputData = inputForm.value.trim();
+    if (!inputData) return;
+
+    const items = getItems();
+    items.push(inputData);
+    saveItems(items);
+    itemlist.insertAdjacentHTML(
+        "afterbegin",
+        `<li>
+        <input type="checkbox" class="item-checkbox">
+        ${inputData}</li>
+        `)
+
     inputForm.value = "";
+
 });
 
 
-
+// チェック付き削除ぼたん
 deleteBtn.addEventListener("click", () => {
-    localStorage.removeItem("standard-item");
+    const checkboxs = document.querySelectorAll(".item-checkbox");
+    const items = getItems();
+    const newItems = [];
+
+    checkboxs.forEach((checkbox, index) => {
+        if (!checkbox.checked) {
+            newItems.push(items[index]);
+        }
+    })
+    saveItems(newItems);
+    itemlist.innerHTML = "";
+    renderInitialList();
 })
+
+renderInitialList();
