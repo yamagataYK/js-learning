03a8e8303368eac3774c5b919d52f842becf5bde
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-06-10 03:08:23 +0900

    コード修正

--- js/practice3.js
+++ js/practice3.js
@@ -1,12 +1,17 @@
 const inputForm = document.querySelector(".input-group__input");
 const saveBtn = document.querySelector(".input-group__savebtn");
 const deleteBtn = document.querySelector(".input-group__deletebtn");
+const itemlist = document.querySelector(".standard-item__list")
+
+
 
 saveBtn.addEventListener("click", () => {
     const inputData = inputForm.value;
     localStorage.setItem("standard-item", inputData);
     inputForm.value = "";
 });
+
+
 
 deleteBtn.addEventListener("click", () => {
     localStorage.removeItem("standard-item");
