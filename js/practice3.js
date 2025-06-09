a0b29d44ea8bce2badf660cd9e95ff4783c8bdaa
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-06-10 02:22:20 +0900

    local storage

---
+++ js/practice3.js
@@ -0,0 +1,13 @@
+const inputForm = document.querySelector(".input-group__input");
+const saveBtn = document.querySelector(".input-group__savebtn");
+const deleteBtn = document.querySelector(".input-group__deletebtn");
+
+saveBtn.addEventListener("click", () => {
+    const inputData = inputForm.value;
+    localStorage.setItem("standard-item", inputData);
+    inputForm.value = "";
+});
+
+deleteBtn.addEventListener("click", () => {
+    localStorage.removeItem("standard-item");
+})
