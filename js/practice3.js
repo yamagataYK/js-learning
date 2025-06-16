aeb9cb40ea19b524a85a8fd980d8d92726947c36
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-06-17 03:53:10 +0900

    モーダルウィンドウの追加

--- js/practice3.js
+++ js/practice3.js
@@ -61,3 +61,22 @@ deleteBtn.addEventListener("click", () => {
 })
 
 firstlist();
+
+//モーダルウインドウ
+const open = document.querySelector("#open")
+const mask = document.querySelector("#mask")
+const modal = document.querySelector(".input-group")
+const close = document.querySelector("#close")
+
+open.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    mask.classList.remove("hidden");
+})
+close.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+})
+mask.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+})
